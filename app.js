67fbb027c217c2dccd2876f166607b6940ef7bc6
Dávid Halász <skateman@skateman.eu>
2016-09-30 12:31:35 +0200

    Added e-mail link+icon to the social bar

--- app.js
+++ app.js
@@ -4,4 +4,34 @@ $(function () {
   $('.twemoji').each(function (_, node) {
     twemoji.parse(node);
   });
+
+  var mapping = {
+    'm':           '(',
+    'a':           '╯',
+    'i':           '°',
+    'l':           '□',
+    't':           '°',
+    'o':           '）',
+    ':':           '╯',
+    'skateman':    '︵',
+    '@':           ' ',
+    'skateman.eu': '┻━┻'
+  }
+
+  $('a:has(.fa-envelope-o)').on({
+    mouseenter: function (e) {
+      var link = $(this).attr('href');
+      $.each(mapping, function (key, value) {
+        link = link.replace(value, key);
+      });
+      $(this).attr('href', link);
+    },
+    mouseleave: function (e) {
+      var link = $(this).attr('href');
+      $.each(mapping, function (key, value) {
+        link = link.replace(key, value);
+      });
+      $(this).attr('href', link);
+    }
+  });
 });
